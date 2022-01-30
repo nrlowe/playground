2788131ad8601f53b67eaf669dad1c7ab8170d54
unknown <nate.lowe.2013@owu.edu>
2022-01-29 23:06:45 -0500

    Adding problems to solve

--- src/main/java/com/lowe/playground/service/ProblemSolutions.java
+++ src/main/java/com/lowe/playground/service/ProblemSolutions.java
@@ -1,5 +1,27 @@
 package com.lowe.playground.service;
 
 public class ProblemSolutions {
-
+	//2.5 Sum Lists - (716) + (592) = 219 --- 617 + 295
+	
+	//2.6 Palindrome, linked list
+	
+	//2.7 Intersection - Linked List
+	
+	//2.8 Loop Detection / Linked List
+	
+	//Stacks and Queues
+	
+	//3.1 Single Array to implement 3 stacks
+	
+	//3.2 Stack that has min function that runs 0(n)
+	
+	//3.3 Stack of plates, once certain threshold is reached, new stack is created - pop and push perform as if single stack
+	// part 2 -> popAt(index) function on specific stack as opposed to treating it like a single stack
+	
+	//3.4 Queue that implements 2 stacks
+	
+	//3.5 Sort Stack - have a minimum values at top , can only use another stack as helper, no other data structure
+	
+	//3.6 FIFO dog and cat stack, can request which animal
+	
 }
