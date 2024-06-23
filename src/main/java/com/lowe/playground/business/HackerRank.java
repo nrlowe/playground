7990e24fb9f4f61fef6280d896e3c98915c94d18
nrlowe <nate.lowe.2013@owu.edu>
2024-06-23 16:57:35 -0400

    Adding 2d array path solution

--- src/main/java/com/lowe/playground/business/HackerRank.java
+++ src/main/java/com/lowe/playground/business/HackerRank.java
@@ -2,11 +2,13 @@ package com.lowe.playground.business;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class HackerRank {
 
-    //Save-Humanity - timeout error on larger test cases
+//Save-Humanity - timeout error on larger test cases
     public void virusIndices(String p, String v) {
         char[] geneArr = p.toCharArray();
         char[] virusArr = v.toCharArray();
@@ -59,12 +61,12 @@ public class HackerRank {
         }
     }
 
-    //Save-Humanity Cleanup
+//Save-Humanity Cleanup
     public void virusIndicesRefactor(String p, String v) {
         //check for indexs for each char in v and map where crossovers occur
     }
 
-    //Minimum Cost
+//Minimum Cost of Equalizing an Array
     public static int findMinimumCost(int[] array) {
         // Sort the array to find the median
         Arrays.sort(array);
@@ -86,4 +88,57 @@ public class HackerRank {
 
         return cost;
     }
+
+//Shortest Path 2-D Array
+    class Cell {
+        int x, y, cost;
+        Cell(int x, int y, int cost) {
+            this.x = x;
+            this.y = y;
+            this.cost = cost;
+        }
+    }
+
+    //Directions Up, right, down, left
+    private final int[] dx = {0, 1, 0, -1};
+    private final int[] dy = {-1, 0, 1, 0};
+
+    public int shortestPath(int[][] grid) {
+        int rowLength = grid.length;
+        int colLength = grid[0].length;
+        int[][] minCost = new int[rowLength][colLength];
+
+        for(int i = 0; i < rowLength; i++){
+            for(int j = 0; j < colLength; j++){
+                minCost[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        PriorityQueue<Cell> queue = new PriorityQueue<>(Comparator.comparing(cell -> cell.cost));
+        queue.offer(new Cell(0,0, grid[0][0]));
+        minCost[0][0] = grid[0][0];
+        while(!queue.isEmpty()){
+            Cell currentCell = queue.poll();
+            int currentx = currentCell.x;
+            int currenty = currentCell.y;
+            if(currentx == rowLength && currenty == colLength){
+                return minCost[currentx][currenty];
+            }
+            for(int i = 0; i < 4; i++){
+                int nextx = dx[i];
+                int nexty = dy[i];
+
+                if(nextx >= 0 && currentx < nextx && rowLength >= 0 && currenty < colLength){ 
+                    int newCost = minCost[currentx][currenty] + grid[nextx][nexty];
+                    if(newCost < minCost[nextx][nexty]){
+                        minCost[nextx][nexty] = newCost;
+                        queue.offer(new Cell(nextx, nexty, newCost));
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+
 }   
