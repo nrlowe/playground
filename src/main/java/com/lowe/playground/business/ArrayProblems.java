10f05d3e00f74c868f862ce3a7d8372e0bb280f2
Nate Lowe <nate.lowe.2013@owu.edu>
2025-08-20 23:01:59 -0400

    Adding new questions

---
+++ src/main/java/com/lowe/playground/business/ArrayProblems.java
@@ -0,0 +1,24 @@
+package com.lowe.playground.business;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class ArrayProblems {
+//Diagonal Differences
+    public int diagonalDifference(List<List<Integer>> arr){
+        int[] diagonalOne = new int[arr.length()];
+        int[] diagonalTwo = new int[arr.get(0).length()];
+
+    }
+
+
+
+
+
+
+
+    
+}
--- src/main/java/com/lowe/playground/business/InterviewQs.java
+++ src/main/java/com/lowe/playground/business/InterviewQs.java
@@ -50,4 +50,59 @@ public class InterviewQs {
         }
         return s.stream().toString();
     }
+
+//MinCore Problem
+    public static int minCoresNeeded(int[] startTimes, int[] endTimes){
+        int n = startTimes.length;
+        int best = 0, current = 0;
+        int s = 0, e = 0;
+        Arrays.sort(startTimes);
+        Arrays.sort(endTimes);
+        while(s < n){
+            if(startTimes[s] < endTimes[e]){
+                current++;
+                best = Math.max(best, current);
+                x++;
+            } else {
+                y++;
+                current--;
+            }
+        }
+        return best;
+    }
+
+//Stable Gene Problem
+    public static int stableGene(String gene){
+        char[] d = {'A', 'C', 'G', 'T'};
+        int n = gene.length();
+        int target = n / 4;
+        int[] freq = new int[128];
+        for(int i = 0; i < n, i++){
+            freq[gene.charAt[i]]++;
+        }
+        int totalN = 0;
+        int[] need = new int[128];
+        for(char ch : d){
+            need[ch] = Math.max(0, freq[ch] - target);
+            totalN += need[ch];
+        }
+        int ans = n;
+        int l = 0;
+        for(int i = 0; i < n; i++){
+            char c = gene.charAt(i);
+            if(need[c] > 0){
+                totalN--;
+            }
+            need[c]--;
+            while(totalN == 0){
+                ans = Math.min(ans, i - l + 1);
+                char cl = gene.charAt(l++);
+                need[cl]++;
+                if(need[cl] > 0){
+                    totalN++;
+                }
+            }
+        }
+        return ans;
+    }
 }
